50c8573abd8e85cb9bcd72a380c436c4dbf4535a
hetpatel07 <hets7878401023@gmail.com>
2019-03-15 19:58:57 +0530

    updated

--- js/main.js
+++ js/main.js
@@ -7,8 +7,18 @@ console.log(tr);
 function sub() {
     var c = tr.parentNode;
     var b = [];
+    var x = [];
     var a = document.createElement('tr');
     c.appendChild(a);
+    for (i = 0; i < 3; i++) {
+        x[0] = document.forms["myForm"]["fname"].value;
+        x[1] = document.forms["myForm"]["fname1"].value;
+        x[2] = document.forms["myForm"]["fname2"].value;
+        if (x[i] == "") {
+            alert("Name must be filled out");
+            return false;
+        }
+    }
     for (i = 0; i < 5; i++) {
         b[i] =
             document.createElement('td');
@@ -21,6 +31,42 @@ function sub() {
     b[3].innerHTML = '<button type="button" class="btn btn-light submit-btn" onclick="pop(this)"id="submitbtn">Update</button>';
     b[4].innerHTML = '<button type="button" class="btn btn-light submit-btn"onclick="deletet(this)">Delete</button>';
 }
+
+var innerlog = document.getElementsByClassName('innerform')[0];
+var logmain = document.getElementsByClassName('logmain')[0];
+
+function login() {
+    console.log(b);
+    logmain.style.opacity = "1";
+    logmain.style.zIndex = "1";
+    logmain.style.transition = ".3s all";
+    innerlog.style.transition = ".3s all";
+    logmain.style.height = "100%";
+    innerlog.style.opacity = "1";
+    innerlog.style.top = "50%";
+    innerlog.style.zIndex = "1";
+    //    window.location.href = "main.html";
+}
+
+function logininner() {
+    var logname = document.getElementById('logname').value;
+    var tdi = document.getElementsByTagName('td');
+    for (i = 0; i < tdi.length; i++) {
+        if (tdi[i].innerHTML == logname) {
+            var windowmain = window.location.href = "main.html";
+            console.log(windowmain);
+            var logtd = document.getElementById('maintr');
+            console.log(logtd);
+        }
+    }
+}
+//
+//function validateForm() {
+//
+
+//function mainpage(){
+//    alert('helo');
+//}
 
 function deletet(del) {
     var delmain = del.parentNode.parentNode;
@@ -149,4 +195,12 @@ function popdown() {
     b.style.opacity = "0";
     b.style.top = "35%";
     b.style.zIndex = "-1";
+    logmain.style.opacity = "0";
+    logmain.style.zIndex = "-1";
+    logmain.style.transition = ".3s all";
+    innerform.style.transition = ".3s all";
+    logmain.style.height = "100%";
+    innerform.style.opacity = "0";
+    innerform.style.top = "35%";
+    innerform.style.zIndex = "-1";
 }
